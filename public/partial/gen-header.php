690a93becf3ec30ff43536887b5ddd10c9bf1bdf
Hannah A. Patellis <hpatellis@icloud.com>
2025-01-16 10:04:11 -0600

    Changes default font to Lato

--- public/partial/gen-header.php
+++ public/partial/gen-header.php
@@ -4,8 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PROlific by Hannah A. Patellis</title>
-    <link href="../asset/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="../asset/css/custom.css" rel="stylesheet" crossorigin="anonymous">
+    <link href="/asset/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="/asset/css/fonts.css" rel="stylesheet" crossorigin="anonymous">
+    <link href="/asset/css/custom.css" rel="stylesheet" crossorigin="anonymous">
   </head>
 
   <body>
